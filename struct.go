e32b6f64eb8cfa45411d1fdd178d478545f83172
Homin Lee <homin.lee@suapapa.net>
2020-05-26 05:57:17 +0900

    add _config.yml

--- struct.go
+++ struct.go
@@ -1,5 +1,32 @@
 package main
 
+// Config contains basic config informations
+type Config struct {
+	Name        string `yaml:"name"`
+	Title       string `yaml:"title"`
+	Title2      string `yaml:"title2"`
+	Description string `yaml:"description"`
+	URL         string `yaml:"url"`
+	Baseurl     string `yaml:"baseurl"`
+	Personal    string `yaml:"personal"`
+	Email       string `yaml:"email"`
+	Twitter     string `yaml:"twitter"`
+	Copyright   string `yaml:"copyright"`
+	Navigation  string `yaml:"navigation"`
+	Analytics   string `yaml:"analytics"`
+	Assets      struct {
+		Dirname       string   `yaml:"dirname"`
+		Baseurl       string   `yaml:"baseurl"`
+		CSSCompressor string   `yaml:"css_compressor"`
+		JsCompressor  string   `yaml:"js_compressor"`
+		Cachebust     string   `yaml:"cachebust"`
+		Cache         bool     `yaml:"cache"`
+		Gzip          string   `yaml:"gzip"`
+		Sources       []string `yaml:"sources"`
+	} `yaml:"assets"`
+	Exclude []string `yaml:"exclude"`
+}
+
 // Sections reperesents resume sections
 type Sections []struct {
 	Name   string `yaml:"name"`
